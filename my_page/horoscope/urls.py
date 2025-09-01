10b965fc54fd09e5d743b72d69a559de98a1ab5f
MacBookPro <alex@gmail.com>
2025-09-01 06:51:08 +0300

    Добавил наследование шаблонов, сделал тег base, include. Разобрал вьюху, удалил лишние фукнции.

--- my_page/horoscope/urls.py
+++ my_page/horoscope/urls.py
@@ -9,15 +9,7 @@ register_converter(converters.UpperConvertor, 'my_upper')
 
 
 urlpatterns = [
-    path('', views.index),
-    path('type/', views.type_index),
-    path('<my_split:sing_zodiac>/', views.get_my_split_converters),
-    path('<my_date:sing_zodiac>/', views.get_my_date_converters),
-    path('type/<str:type_name>/', views.type, name='type_name'),
-    path('<yyyy:sing_zodiac>/', views.get_yyyy_converters),
-    path('<int:sing_zodiac>/', views.get_info_number),
-    path('<my_float:sing_zodiac>/', views.get_my_float_converters),
-    path('<str:sing_zodiac>/', views.get_info, name='horoscope-name'),
-    path('<int:month>/<int:day>/', views.horoscope_by_date, name='horoscope_by_date'),
-    path('<my_upper:sing_zodiac>/', views.get_my_upper_converters),
+    path('', views.index, name='horoscope-name'),
+    path('<int:sign_zodiac>/', views.get_info_number),
+    path('<str:sign_zodiac>/', views.get_info, name='horoscope-name'),
 ]
--- my_page/horoscope/views.py
+++ my_page/horoscope/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import  render_to_string
-import calendar
-from dataclasses import dataclass
 
-
-signs = {
+zodiac_dict = {
     "aries": "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).",
     "taurus": "Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).",
     "gemini": "Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).",
@@ -21,72 +17,26 @@ signs = {
     "pisces": "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)."
 }
 
-types_dict = {
-    'fire': ['aries', 'leo', 'sagittarius'],
-    'earth': ['taurus', 'virgo', 'capricorn'],
-    'air': ['gemini', 'libra', 'aquarius'],
-    'water': ['cancer', 'scorpio', 'pisces']
-}
-
-zodiac_dates = {
-    ('capricorn', (12, 22), (1, 19)),
-    ('aquarius', (1, 20), (2, 18)),
-    ('pisces', (2, 19), (3, 20)),
-    ('aries', (3, 21), (4, 19)),
-    ('taurus', (4, 20), (5, 20)),
-    ('gemini', (5, 21), (6, 20)),
-    ('cancer', (6, 21), (7, 22)),
-    ('leo', (7, 23), (8, 22)),
-    ('virgo', (8,23), (9, 22)),
-    ('libra', (9, 23), (10, 22)),
-    ('scorpio', (10, 23), (11, 21)),
-    ('sagittarius', (11, 22), (12, 21))
-}
-
-def get_yyyy_converters(request, sing_zodiac):
-    return HttpResponse(f'Вы передали число из 4х чисел - {sing_zodiac}')
-
-def get_my_float_converters(request, sing_zodiac):
-    return HttpResponse(f'Вы передали вещественное число- {sing_zodiac}')
-
-
-def get_my_date_converters(request, sing_zodiac):
-    return HttpResponse(f'Вы передали год- {sing_zodiac}')
-
-def get_my_split_converters(request, sing_zodiac):
-    return HttpResponse(f'Возвращаем список строк- {sing_zodiac}')
-
-def get_my_upper_converters(request, sing_zodiac):
-    return HttpResponse(f'Возвращаем строку с изменением регистра- {sing_zodiac}')
-
-
 def index(request):
-    zodiacs = list(signs)
+    zodiacs = list(zodiac_dict)
     #f'<li> <a href="{redirect_url}">{sing.title()} </a> </li>'
     context = {
-        'zodiacs': zodiacs
+        'zodiacs': zodiacs,
+        'zodiac_dict': {},
     }
     return render(request, 'horoscope/index.html', context=context)
 
-@dataclass
-class Person:
-    name: str
-    age: int
 
-    def __str__(self):
-        return  f'This is {self.name}'
-
-# Create your views here.
-def get_info(request, sing_zodiac: str):
-    description = signs.get(sing_zodiac.lower())
+def get_info(request, sign_zodiac: str):
+    description = zodiac_dict.get(sign_zodiac)
     data = {
         'description_zodiac': description,
-        'sign': sing_zodiac.title,
+        'sign': sign_zodiac,
     }
     return render(request, 'horoscope/info_zodiac.html', context=data)
 
 def get_info_number(request, sing_zodiac: int):
-    zodiacs = list(signs)
+    zodiacs = list(zodiac_dict)
     if sing_zodiac > len(zodiacs):
         return HttpResponse(f'Неправильный порядковый номер знака зодиака - {sing_zodiac}')
     name_zodiac = zodiacs[sing_zodiac - 1]
@@ -94,33 +44,3 @@ def get_info_number(request, sing_zodiac: int):
     return HttpResponseRedirect(redirect_urls)
     redirect_url = reverse('horoscope-name', args=(name_zodiac, ))
     return HttpResponseRedirect(redirect_url)
-
-
-def type_index(request):
-    li_elements = ''
-    for type in types_dict:
-        li_elements += f'<li> <a href = "{type}/"> {type.title()} </a> </li>'
-    return HttpResponse(f'<ol> {li_elements} </ol>')
-
-def type(request, type_name):
-    li_elements = ''
-    for sing in types_dict[type_name]:
-        redirect_path = reverse('horoscope-name', args=[sing])
-        li_elements += f'<li> <a href= "{redirect_path}"> {sing.title()} </a> </li>'
-    return  HttpResponse(f'<ol> {li_elements} </ol>')
-
-def horoscope_by_date(request, month, day):
-    if month < 1 or month > 12:
-        return HttpResponse('Неверный месяц')
-    if day < 1 or day > calendar.monthrange(2025, month)[1]:
-        return HttpResponse('Неверный день для этого месяца')
-
-    for sign, (start_month, start_day), (end_month, end_day) in zodiac_dates:
-        if (start_month == end_month and start_day <= day <= end_day and month == start_month) or \
-                (start_month < end_month and (
-                        (month == start_month and day >= start_day) or (month == end_month and day <= end_day))) or \
-                (start_month > end_month and (
-                        (month == start_month and day >= start_day) or (month == end_month and day <= end_day))):
-            return HttpResponse(f"Знак зодиака: {sign.title()}")
-
-    return HttpResponse("Неверная дата")
