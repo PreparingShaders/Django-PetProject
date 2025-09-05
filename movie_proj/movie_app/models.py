27956fb58017283ff79e3c0ad138aa18f7536a16
MacBookPro <alex@gmail.com>
2025-09-05 06:55:41 +0300

    Сделал БД, выполнил миграцию в БД, проверил все работает.

--- movie_proj/movie_app/models.py
+++ movie_proj/movie_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Movive(models.Model):
+class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
 
