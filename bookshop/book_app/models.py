ff3d7835f7cf927bfebdf75ab556e490a3093050
MacBookPro <alex@gmail.com>
2025-09-16 06:54:02 +0300

    Добавил записи в 2 БД, переопределил метод __str__

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title} рейтинг {self.rating}'
