8f645ea69df6a27d007c21dae8df765c07f6ad0c
MacBookPro <alex@gmail.com>
2025-09-24 07:22:34 +0300

    Установил дополнительный пакет debag для Django, прошел агрегатные фукнции.

--- movie_proj/movie_app/views.py
+++ movie_proj/movie_app/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import  F, Sum, Max, Min, Count, Avg, Value
 from .models import Movie
 # Create your views here.
 
 
 def show_all_movie(request):
-    movies = Movie.objects.all()
+    #movies = Movie.objects.order_by(F('year').asc(nulls_last=True))
+    movies = Movie.objects.annotate(true_bool = Value(True),
+                                    false_bool = Value(False),
+                                    str_field = Value('Hello'),
+                                    new_budget = F('budget') + 100,
+                                    rating_year = F('rating') + F('year')
+                                    )
+    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('id'))
     return render(request, 'movie_app/all_movie.html', {
-        'movies': movies})
+        'movies': movies,
+         'agg': agg})
 
 def show_one_movie(request, id_movie:int):
     movie = get_object_or_404(Movie, id=id_movie)
