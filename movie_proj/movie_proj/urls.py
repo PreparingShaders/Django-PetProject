f9863e45a2784f14fd1ea5d5b392612679334415
MacBookPro <alex@gmail.com>
2025-09-25 06:58:22 +0300

    Настройка админки Django, изменение заголовков

--- movie_proj/movie_proj/urls.py
+++ movie_proj/movie_proj/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
+admin.site.site_header = 'Наша Админка'
+admin.site.index_title = 'Моя супер Админка'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
