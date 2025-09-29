282c32ad56c1d565cf061065ee4f2fc7da62785c
MacBookPro <alex@gmail.com>
2025-09-29 08:15:38 +0300

    Добавил поле currency, сделал миграиции, добавил действие ввиде приведения всех параметров к одной валюте

--- movie_proj/movie_app/admin.py
+++ movie_proj/movie_app/admin.py
@@ -1,7 +1,31 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Movie
+from django.db.models import QuerySet
 # Register your models here.
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'budget', 'year']
-    list_editable = ['year', 'budget']
+    list_display = ['name', 'rating', 'budget', 'currency', 'ratin_status']
+    list_editable = ['currency', 'budget', 'rating']
+    ordering = ['-rating', '-name']
+    list_per_page = 10
+    actions = ['set_dollars', 'set_euro']
+
+    @admin.display(ordering='rating', description='Статус')
+    def ratin_status(self, movie: Movie):
+        if movie.rating < 50:
+            return 'Зачем это смотреть?'
+        if movie.rating < 70:
+            return 'Разок можно глянуть'
+        if movie.rating <= 85:
+            return 'Зачет'
+        return 'Топчик'
+
+    @admin.action(description='Установить валюту в долларах')
+    def set_dollars(self, request, qs:QuerySet ):
+        qs.update(currency= Movie.USD)
+
+    @admin.action(description='Установить валюту в евро')
+    def set_euro(self, request, qs:QuerySet ):
+        count_updated = qs.update(currency= Movie.EUR)
+        self.message_user(request,f'Было обновлено {count_updated}',
+                          messages.ERROR)
