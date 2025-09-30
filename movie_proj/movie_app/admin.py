912cafded552ff40704f96165d7cd65b6f01532b
MacBookPro <alex@gmail.com>
2025-09-30 06:53:00 +0300

    Создал новый класс Directors, и настроил slug

--- movie_proj/movie_app/admin.py
+++ movie_proj/movie_app/admin.py
@@ -1,14 +1,46 @@
 from django.contrib import admin, messages
-from .models import Movie
+from .models import Movie, Directors
 from django.db.models import QuerySet
 # Register your models here.
+
+
+admin.site.register(Directors)
+
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', 'Низкий рейтинг'),
+            ('от 40 до 59', 'Средний'),
+            ('от 60 до 79', 'Высокий'),
+            ('>=80', 'Высочайший'),
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<=40':
+            return queryset.filter(rating__lt =40)
+        if self.value() == 'от 40 до 59':
+            return queryset.filter(rating__gte=40).filter(rating__lt=60)
+        if self.value() == 'от 60 до 79':
+            return queryset.filter(rating__gte=60).filter(rating__lt=79)
+        if self.value() == '>=80':
+            return queryset.filter(rating__gte=80)
+
+        return queryset
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
+    # fields = ['name', 'rating']
+    prepopulated_fields = {'slug': ('name', )}
     list_display = ['name', 'rating', 'budget', 'currency', 'ratin_status']
     list_editable = ['currency', 'budget', 'rating']
     ordering = ['-rating', '-name']
     list_per_page = 10
     actions = ['set_dollars', 'set_euro']
+    list_filter = ['name', 'currency', RatingFilter]
 
     @admin.display(ordering='rating', description='Статус')
     def ratin_status(self, movie: Movie):
--- movie_proj/movie_app/models.py
+++ movie_proj/movie_app/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Directors(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    director_email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name}  {self.last_name}'
+
+
 class Movie(models.Model):
     EUR = 'EUR'
     USD = 'USD'
@@ -14,16 +25,14 @@ class Movie(models.Model):
     ]
 
     name = models.CharField(max_length=40)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1),
+                                             MaxValueValidator(100)])
     year = models.IntegerField(null=True, blank=True)
-    budget = models.IntegerField(default=1000000)
+    budget = models.IntegerField(default=1000000, validators=[MinValueValidator(1)])
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default='', null=False, db_index=True)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Movie, self).save(*args, **kwargs)
-
+    director = models.CharField(max_length=100, default='Квентин Тарантино')
+    director_email = models.EmailField(default='sugar_daddy@gmail.com')
     def get_url(self):
         return reverse('movie-detail', args=[self.slug])
 
